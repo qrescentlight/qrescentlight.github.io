ddae68987fdadeeff90154163079881878cbdfe7
qrescentlight <100691142+qrescentlight@users.noreply.github.com>
2026-09-25 14:03:30 +0200

    Add files via upload

---
+++ bosinhuis/AR/js/foo-component.js
@@ -0,0 +1,9 @@
+AFRAME.registerComponent('foo', {
+    schema: {},
+    init: function () {},
+    update: function () {},
+    tick: function () {},
+    remove: function () {},
+    pause: function () {},
+    play: function () {}
+  });
